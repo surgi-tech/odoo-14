e79623e2600d1cd962d99cd395f8b7f9b5b568d4
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-10-04 16:05:03 +0200

    account_journal_security & multi_write_off_advanced & surgi_accounting

--- account_journal_security/models/account_payment.py
+++ account_journal_security/models/account_payment.py
@@ -1,14 +1,7 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
 from odoo import fields, models
-
 
 class AccountPayment(models.Model):
 
     _inherit = 'account.payment'
 
-    journal_id = fields.Many2one(
-        auto_join=True,
-    )
+    journal_id = fields.Many2one(comodel_name="account.journal",auto_join=True,)
